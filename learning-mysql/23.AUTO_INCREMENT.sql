ce3d5df902f756ed9f19f443a38dd1e2a0d9287e
Ishan-Kanishka <ikanishka368@gmail.com>
2025-09-08 17:28:02 +0530

    renamed some files

---
+++ learning-mysql/23.AUTO_INCREMENT.sql
@@ -0,0 +1,9 @@
+# TABLE transactions (
+#    transaction_id INT PRIMARY KEY AUTO_INCREMENT = 1000,
+#    amount DECIMAL (5,2)
+# );
+
+INSERT INTO transactions (amount)
+VALUES (5.98);
+# NULL  NULL do not mind it bitttttchhhhhhhh!!!!!!
+SELECT * FROM transactions;
